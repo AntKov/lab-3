e60ef7379ec3e2d64808da42bd30b1a04d2ca7a4
AntKov <anton.beskyd@gmail.com>
2023-12-01 18:06:20 +0200

    small test

--- calc.py
+++ calc.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     a = float(input("Enter the first number: "))
     b = float(input("Enter the second number: "))
     result = add(a, b)
-    print(f"The sum of  {a} and {b} is: {result}")
+    print(f"The  sum of  {a} and {b} is: {result}")
